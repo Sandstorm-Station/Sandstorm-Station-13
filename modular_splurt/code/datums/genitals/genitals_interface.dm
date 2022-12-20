6f396825960baaf3a3b8fe21274da47321844afa
Pin-alternative <pinpenpacked@gmail.com>
2022-12-20 21:17:50 +1100

    ui

---
+++ modular_splurt/code/datums/genitals/genitals_interface.dm
@@ -0,0 +1,175 @@
+/// Attempts to open the tgui menu
+/mob/living/verb/configure_genitals()
+	set name = "Genitals Menu"
+	set desc = "Change your genital settings"
+	set category = "IC"
+
+	if(!iscarbon(usr))
+		return
+	if(!usr.mind) //Mindless boys, honestly just don't, it's better this way
+		return
+	if(!usr.mind.genitals_menu_holder)
+		usr.mind.genitals_menu_holder= new(usr.mind)
+
+	usr.mind.genitals_menu_holder.ui_interact(usr)
+
+/datum/mind
+	var/datum/genitals_menu/genitals_menu_holder
+
+/datum/mind/New(key)
+	. = ..()
+	genitals_menu_holder = new(src)
+
+/datum/genitals_menu
+
+/datum/genitals_menu/ui_state(mob/user)
+	return GLOB.conscious_state
+
+/datum/genitals_menu/ui_interact(mob/user, datum/tgui/ui)
+	ui = SStgui.try_update_ui(user, src, ui)
+	if(!ui)
+		ui = new(user, src, "GenitalConfig", "Genitals")
+		ui.open()
+
+
+/datum/genitals_menu/ui_data(mob/user)
+	. = ..()
+	var/mob/living/carbon/genital_holder = user
+	var/list/genitals = list()
+	for(var/obj/item/organ/genital/genital in genital_holder.internal_organs)	//Only get the genitals
+		if(CHECK_BITFIELD(genital.genital_flags, GENITAL_INTERNAL))			//Not those though
+			continue
+		var/list/genital_entry = list()
+		genital_entry["name"] = "[capitalize(genital.name)]" //Prevents code from adding a prefix
+		genital_entry["key"] = REF(genital) //The key is the reference to the object
+
+		genital_entry["description"] = genital.desc + "\n [genital.linked_organ ? "Linked organ: [genital.linked_organ.name]" : ""]"
+
+		var/visibility = "Invalid"
+		if(CHECK_BITFIELD(genital.genital_flags, GENITAL_THROUGH_CLOTHES))
+			visibility = "Always visible"
+		else if(CHECK_BITFIELD(genital.genital_flags, GENITAL_UNDIES_HIDDEN))
+			visibility = "Hidden by underwear"
+		else if(CHECK_BITFIELD(genital.genital_flags, GENITAL_HIDDEN))
+			visibility = "Always hidden"
+		else
+			visibility = "Hidden by clothes"
+
+		var/extras = "None"
+		if(CHECK_BITFIELD(genital.genital_flags, GENITAL_CAN_STUFF))
+			extras = "Allows egg stuffing"
+
+		genital_entry["extras"] = extras
+		genital_entry["visibility"] = visibility
+		genital_entry["possible_choices"] = GLOB.genitals_visibility_toggles
+		genital_entry["extra_choices"] = list(GEN_ALLOW_EGG_STUFFING)
+		genital_entry["can_arouse"] = (
+			!!CHECK_BITFIELD(genital.genital_flags, GENITAL_CAN_AROUSE) \
+			&& !(HAS_TRAIT(genital_holder, TRAIT_PERMABONER) \
+			|| HAS_TRAIT(genital_holder, TRAIT_NEVERBONER)))
+		genital_entry["arousal_state"] = genital.aroused_state
+		genital_entry["max_size"] = 5 //TODO
+
+
+		//fluids
+		if(CHECK_BITFIELD(genital.genital_flags, GENITAL_FUID_PRODUCTION))
+			var/fluids = (clamp(genital.fluid_rate * ((world.time - genital.last_orgasmed) / (10 SECONDS)) * genital.fluid_mult, 0, genital.fluid_max_volume) / genital.fluid_max_volume)
+			genital_entry["fluid"] = fluids
+
+		//equipments
+		if(genital.is_exposed())
+			var/list/equipments = list()
+			for(var/obj/equipment in genital.contents)
+				var/list/equipment_entry = list()
+				equipment_entry["name"] = equipment.name
+				equipment_entry["key"] = REF(equipment)
+				equipments += list(equipment_entry)
+			genital_entry["possible_equipment_choices"] = GLOB.genitals_interactions
+			genital_entry["equipments"] = equipments
+
+		genitals += list(genital_entry)
+
+	if(!genital_holder.getorganslot(ORGAN_SLOT_ANUS))
+		var/simulated_ass = list()
+		simulated_ass["name"] = "Anus"
+		simulated_ass["key"] = "anus"
+		var/visibility = "Invalid"
+		switch(genital_holder.anus_exposed)
+			if(1)
+				visibility = "Always visible"
+			if(0)
+				visibility = "Hidden by underwear"
+			else
+				visibility = "Always hidden"
+		simulated_ass["visibility"] = visibility
+		simulated_ass["possible_choices"] = GLOB.genitals_visibility_toggles - GEN_VISIBLE_NO_CLOTHES
+		genitals += list(simulated_ass)
+	.["genitals"] = genitals
+
+/datum/genitals_menu/ui_act(action, list/params, datum/tgui/ui, datum/ui_state/state)
+	if(..())
+		return
+	switch(action)
+		if("genital")
+			var/mob/living/carbon/self = usr
+			if("visibility" in params)
+				if(params["genital"] == "anus")
+					self.anus_toggle_visibility(params["visibility"])
+					return TRUE
+				var/obj/item/organ/genital/genital = locate(params["genital"], self.internal_organs)
+				if(genital && (genital in self.internal_organs))
+					genital.toggle_visibility(params["visibility"])
+					return TRUE
+			if("set_arousal" in params)
+				var/obj/item/organ/genital/genital = locate(params["genital"], self.internal_organs)
+				if(!genital || (genital \
+					&& (!CHECK_BITFIELD(genital.genital_flags, GENITAL_CAN_AROUSE) \
+					|| HAS_TRAIT(self, TRAIT_PERMABONER) \
+					|| HAS_TRAIT(self, TRAIT_NEVERBONER))))
+					return FALSE
+				var/original_state = genital.aroused_state
+				genital.set_aroused_state(params["set_arousal"])// i'm not making it just `!aroused_state` because
+				if(original_state != genital.aroused_state)		// someone just might port skyrat's new genitals
+					to_chat(self, "<span class='userlove'>[genital.aroused_state ? genital.arousal_verb : genital.unarousal_verb].</span>")
+					. = TRUE
+				else
+					to_chat(self, "<span class='userlove'>You can't make that genital [genital.aroused_state ? "unaroused" : "aroused"]!</span>")
+					. = FALSE
+				genital.update_appearance()
+				if(ishuman(self))
+					var/mob/living/carbon/human/human = self
+					human.update_genitals()
+				return
+			else
+				return FALSE
+		if("equipment")
+			var/mob/living/carbon/self = usr
+			var/obj/item/organ/genital/genital = locate(params["genital"], self.internal_organs)
+			if(!(genital && (genital in self.internal_organs)))
+				return FALSE
+			switch(params["equipment_action"])
+				if("remove")
+					var/obj/item/selected_item = locate(params["equipment"], genital.contents)
+					if(selected_item)
+						if(!do_mob(self, self, 5 SECONDS))
+							return FALSE
+						if(!self.put_in_hands(selected_item))
+							self.transferItemToLoc(get_turf(self))
+						return TRUE
+					return FALSE
+				if("insert")
+					var/obj/item/stuff = self.get_active_held_item()
+					if(!istype(stuff))
+						to_chat(self, span_warning("You need to hold an item to insert it!"))
+						return FALSE
+					stuff.insert_item_organ(self, self, genital)
+		if("sizes")
+			var/mob/living/carbon/self = usr
+			var/obj/item/organ/genital/genital = locate(params["genital"], self.internal_organs)
+			if(!(genital && (genital in self.internal_organs)))
+				return FALSE
+			//switch(params["action"])
+			//	if("max")
+
+
+
---
+++ tgui/packages/tgui/interfaces/GenitalConfig.tsx
@@ -0,0 +1,258 @@
+
+import { filter } from 'common/collections';
+import { flow } from 'common/fp';
+import { createSearch } from 'common/string';
+import { useBackend, useLocalState } from '../backend';
+import { BlockQuote, Button, LabeledList, Icon, NumberInput, Input, Section, Table, Tabs, Stack, ProgressBar, Divider } from '../components';
+import { Window } from '../layouts';
+
+
+type GenitalInfo = {
+  genitals: GenitalData[];
+}
+
+type GenitalData = {
+  name: string,
+  key: string,
+  description: string,
+  visibility: string,
+  extras: string,
+  extra_choices: string[],
+  possible_choices: string[],
+  can_arouse: boolean,
+  arousal_state: boolean,
+  fluid: number,
+  possible_equipment_choices: string[],
+  equipments: Equipment[],
+}
+
+type Equipment = {
+  name: string,
+  key: string,
+}
+
+export const GenitalConfig = (props, context) => {
+  const { act, data } = useBackend<GenitalInfo>(context);
+  const genitals = data.genitals || [];
+  const [tabIndex, setTabIndex] = useLocalState(context, 'tabIndex', 0)
+  return (
+    <Window
+      width={850}
+      height={600}
+      theme="hotpink"
+      resizable>
+      <Window.Content scrollable={false}>
+        {genitals.length ? (
+        <>
+        <Section title="Genital">
+          <Stack vertical>
+          <Stack.Item>
+          <Tabs fluid textAlign="center">
+            {genitals.map((genital, index) => (
+              <Tabs.Tab
+                selected={tabIndex === index}
+                onClick={() => setTabIndex(index)}
+              >
+                {genital.name}
+              </Tabs.Tab>
+            ))}
+          </Tabs>
+          </Stack.Item>
+          <Stack.Item>
+          {genitals[tabIndex].description}
+          </Stack.Item>
+          </Stack>
+        </Section>
+        <Section title="Settings">
+          <Stack grow>
+            <Stack.Item grow>
+              <Stack vertical>
+                <Stack.Item>
+                  <Section>
+                    Fluid level:
+                    <ProgressBar
+                      key={genitals[tabIndex].key}
+                      value={genitals[tabIndex].fluid ? genitals[tabIndex].fluid : 0.0}
+                      color="white"
+                    />
+                  </Section>
+                </Stack.Item>
+                <Stack.Item>
+                  <Stack grow>
+                    <Stack.Item>
+                      <SizeButtons />
+                    </Stack.Item>
+                    <Stack.Item grow>
+                      <ToggleSettings />
+                    </Stack.Item>
+                  </Stack>
+                </Stack.Item>
+              </Stack>
+            </Stack.Item>
+          </Stack>
+        </Section>
+        <Section
+          title="Items Inserted"
+          buttons={(
+            <Button
+              fluid
+              content="Insert Item"
+              onClick={() => act('equipment', {
+                genital: genitals[tabIndex].key,
+                equipment_action: "insert",
+              })}
+            />
+          )}
+        >
+          <Equipments />
+        </Section>
+        </>
+        ) : (
+          <Section align="center">
+            You don&apos;t seem to have any genitals...
+            Or any that you could modify.
+          </Section>
+        )
+      }
+      </Window.Content>
+    </Window>
+  );
+};
+
+const SizeButtons = (props, context) => {
+  return (
+    <Section>
+      <Stack>
+        <Stack.Item>
+          <Stack vertical>
+            Max growth:
+            <NumberInput />
+          </Stack>
+        </Stack.Item>
+        <Stack.Item>
+          <Stack vertical>
+            Min shrink:
+            <NumberInput />
+          </Stack>
+        </Stack.Item>
+      </Stack>
+    </Section>
+  );
+};
+
+const ModeToIcon = {
+  "Always visible": "eye",
+  "Hidden by clothes": "tshirt",
+  "Hidden by underwear": "low-vision",
+  "Always hidden": "eye-slash",
+  "Allows egg stuffing": "egg",
+};
+
+const ToggleSettings = (props , context) => {
+  const [tabIndex] = useLocalState(context, 'tabIndex', 0)
+  const { act, data } = useBackend<GenitalInfo>(context);
+  const genital = data.genitals[tabIndex];
+  return (
+    <Stack grow>
+      {genital.possible_choices.map(choice => (
+        <Stack.Item grow>
+          <Button
+            textAlign="center"
+            key={choice}
+            tooltip={choice}
+            icon={ModeToIcon[choice]}
+            color={genital.visibility === choice ? "green" : "default"}
+            onClick={() => act('genital', {
+              genital: genital.key,
+              visibility: choice,
+            })}
+            fluid
+          />
+        </Stack.Item>
+      ))}
+      <Stack.Item grow>
+        <Button
+          textAlign="center"
+          key={genital.arousal_state}
+          tooltip={genital.can_arouse
+            ? ((genital.arousal_state ? "Unarouse" : "Arouse") + " your " + genital.name.toLowerCase())
+            : "You cannot modify arousal on your " + genital.name.toLowerCase()}
+          icon="heart"
+          color={genital.can_arouse ? (genital.arousal_state ? "green" : "default") : "grey"}
+          onClick={() => act('genital', {
+            genital: genital.key,
+            set_arousal: !genital.arousal_state,
+          })}
+          fluid
+        />
+      </Stack.Item>
+      {genital.extra_choices instanceof Array
+        ? genital.extra_choices.map(choice => (
+        <Stack.Item grow>
+          <Button
+            textAlign="center"
+            key={choice}
+            tooltip={choice}
+            icon={ModeToIcon[choice]}
+            color={genital.extras === choice ? "green" : "default"}
+            onClick={() => act('genital', {
+              genital: genital.key,
+              visibility: choice,
+            })}
+            fluid
+          />
+        </Stack.Item>
+        )) : null}
+    </Stack>
+  );
+};
+
+const Equipments = (props , context) => {
+  const [tabIndex] = useLocalState(context, 'tabIndex', 0)
+  const {act, data} = useBackend<GenitalInfo>(context)
+  const genital = data.genitals[tabIndex];
+  const [searchText, setSearchText] = useLocalState(context, 'searchText', '');
+  const items = prepareSearch(genital.equipments, searchText) || [];
+  return (
+    <Stack vertical>
+      <Stack.Item>
+        <Input
+          fluid
+          placeholder="Search for items.."
+          onInput={(e, value) => setSearchText(value)}
+        />
+      </Stack.Item>
+      <Stack.Item>
+        <Table mb={1}>
+          {items.map(item =>
+          <Table.Row className="candystripe">
+            <Table.Cell bold>
+              {item.name}
+            </Table.Cell>
+            <Table.Cell collapsing textAlign="center">
+              <Button
+                fluid
+                content="Remove"
+                onClick={() => act('equipment', {
+                  equipment: item.key,
+                  genital: genital.key,
+                  equipment_action: "remove",
+                })}
+              />
+            </Table.Cell>
+          </Table.Row>
+          )}
+        </Table>
+      </Stack.Item>
+    </Stack>
+  );
+};
+
+export const prepareSearch = (items, searchText = '') => {
+  const testSearch = createSearch<Equipment>(searchText,
+    item => item.name);
+  return flow([
+    // Optional search term
+    searchText && filter(testSearch),
+  ])(items);
+};
